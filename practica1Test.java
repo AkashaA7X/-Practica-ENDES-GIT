fe05df552bdfb7dc568514a8d6157ef7fad96264
AkashaA7X <62474757+AkashaA7X@users.noreply.github.com>
2021-04-14 22:26:30 +0200

    Junit
    Se sube el archivo de Test de Junit

---
+++ practica1Test.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class practica1Test {
+
+	@Test
+	public void testMain() {
+		int x=50;
+		int y=30;
+		int media=x+y/2;
+	}
+
+}
